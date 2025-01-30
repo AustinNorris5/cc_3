08cf27f93fc8cbb3b48a8b5413635f5507df48cd
AustinNorris5 <ajnorris@usf.edu>
2025-01-30 11:56:15 -0500

    Task 1 - Product Price Management

---
+++ cc_3.js
@@ -0,0 +1,8 @@
+//Task 1: Product Price Management Scenario
+let prices = [9.99, 19.99, 24.99, 4.99, 49.99]; //Declare and initialize the array with five numerical values
+
+prices.push(18.25); //Add a new price to the array
+
+prices.shift(); //Remove the first price from the array
+
+console.log(prices); //Log the updated price list to the console
